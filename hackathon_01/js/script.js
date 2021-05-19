45b70e4ac991893e9b104c179f41bae05c3febd0
Venkatakrishnan R <69761301+Krish90s@users.noreply.github.com>
2021-05-19 13:59:20 +0530

    Add files via upload

---
+++ hackathon_01/js/script.js
@@ -0,0 +1,16 @@
+//fetch('https://domain.freshdesk.com/api/v2/tickets', {
+    //method: "GET",
+    //headers: {
+       // "X-Auth-Token": "g9OB9tXmwzJtoGj1b0QX",
+       // X:  "g9OB9tXmwzJtoGj1b0QX",
+        //g9OB9tXmwzJtoGj1b0QX:X,
+       // "Content-Type": "application/json",
+   // },
+//})
+//.then(function (data){
+    //return data.json();
+//})
+
+//.catch(function (err){
+    //console.log(err);
+});
